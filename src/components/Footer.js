bf4c40687e552c79daf534467544fd6d541b7972
Rafe Murray <rafemurr@gmail.com>
2026-02-04 00:13:35 -0800

    Update copyright

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,8 @@
-export default function Footer(){
-    return(
-        <div className="w-full flex flex-row justify-center">
-            <p className="text-gray-600 dark:text-gray-50 p-4">© 2024 Rafe Murray</p>
-        </div>
-    );
+export default function Footer() {
+  return (
+    <div className="w-full flex flex-row justify-center">
+      <p className="text-gray-600 dark:text-gray-50 p-4">© 2026 Rafe Murray</p>
+    </div>
+  );
 }
+
